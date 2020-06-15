c60bb4d6d3ffd683cb11801b283660098f1401af
Crucciatus <juliocesarafs2@gmail.com>
2020-06-15 20:44:30 -0300

    challenge-1-4.js: add function validateTransaction

--- challengesJS/challenge-1-4.js
+++ challengesJS/challenge-1-4.js
@@ -4,23 +4,44 @@ const user = {
   balance: 0
 };
 
-function createTransaction(transaction) {
+
+/**
+ * return true if the transactionType is credit or debit
+ */
+function validateTransaction(transactionType) {
+  let isValidTransaction = true;
+  console.log(transactionType)
   try {
 
-    if (transaction.type !== 'credit' && transaction.type !== 'debit')
+    if (transactionType !== 'credit' && transactionType !== 'debit')
       throw err;
-    else {
-      user.transactions.push(transaction);
-      
-      if (transaction.type == 'credit') user.balance += transaction.value;
-      else user.balance -= transaction.value;
-    }  
-  
+
   } catch(err) {
+      isValidTransaction = false;
       console.log('invalid transaction type (only credit or debit)');
-    } 
+    }
+  
+  return isValidTransaction;
+}
+
+
+/**
+ * create a transaction
+ */
+function createTransaction(transaction) {
+  const isValid = validateTransaction(transaction.type);
+  
+  if (isValid) {
+    user.transactions.push(transaction);
+    if (transaction.type == 'credit') user.balance += transaction.value;
+    else user.balance -= transaction.value;
+  }
   
   return;
+}
+
+function getHigherTransactionByType(transactionType) {
+
 }
 
 createTransaction({ type: 'credit', value: 50.5 });

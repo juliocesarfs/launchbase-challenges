099c41a307603f788f622514476e9cdd4ddfe07e
Crucciatus <juliocesarafs2@gmail.com>
2020-06-27 00:27:43 -0300

    routers server finished | challenge 3-3 already finished

--- challenges-fullstack/public/scripts.js
+++ challenges-fullstack/public/scripts.js
@@ -1,43 +1,11 @@
-const modalOverlay = document.querySelector('.modal-overlay');
 const courses = document.querySelectorAll('.course');
-const modal = document.querySelector('.modal');
 
-
-//active modelOverlay function
 for (let course of courses)  {
   course.addEventListener('click', function() {
-    modalOverlay.classList.add('active');
-
     const pageId = course.getAttribute('id');
+    
+    window.location.href = `/courses/${pageId}`;
 
-    modalOverlay.querySelector('iframe').src = `https://rocketseat.com.br/${pageId}`;
   });
 }
-
-
-//close modelOverlay function
-document.querySelector('.close-modal').addEventListener('click', function() {
-  
-  modalOverlay.classList.remove('active');
-  modalOverlay.querySelector('iframe').src = '';
-  modal.classList.remove('maximize');
-  document.querySelector('#fullscreen').textContent = 'fullscreen';
-
-});
-
-
-//maximize modelOverlay function
-document.querySelector('.maximize-modal').addEventListener('click', function() {                                  
-  if(modal.classList.contains('maximize')) {
-    modal.classList.remove('maximize');
-
-    document.querySelector('#fullscreen').textContent = 'fullscreen';
-  }
-  else {
-    modal.classList.add('maximize');
-
-    document.querySelector('#fullscreen').textContent = 'fullscreen_exit';
-  }
-  
-});
 

f968aa2c38db6df2c06d115cf2d72109f3211a93
Crucciatus <juliocesarafs2@gmail.com>
2020-07-20 20:47:06 -0300

    fix images

--- challenges-fullstack/challenge-portfolio/data.js
+++ challenges-fullstack/challenge-portfolio/data.js
@@ -2,7 +2,7 @@ module.exports = [
   {
     id: 'zero',
     name: 'starter',
-    image: 'https://skylab.rocketseat.com.br/static/64c237ccff807c054339a62d53b4b402.svg',
+    image: '/images/starter.svg',
     description: 'Torne-se um programador desejado</p> no mercado com esses cursos gratuitos',
     modules: '5',
     price: 'Free',
@@ -17,7 +17,7 @@ module.exports = [
   {
     id: 'one',
     name: 'launchbase',
-    image: 'https://skylab.rocketseat.com.br/static/0828532024cb46921a6b5e941f8d788d.svg',
+    image: '/images/launchbase.svg',
     description: 'Domine a programação do zero</p> e tenha acesso às melhores oportunidades do mercado',
     modules: '16',
     price: 'Pago',
@@ -32,7 +32,7 @@ module.exports = [
   {
     id: 'two',
     name: 'gostack',
-    image: 'https://skylab.rocketseat.com.br/static/83a178a0653dab1d55e2ed7946465975.svg',
+    image: '/images/gostack.svg',
     description: 'Treinamento imersivo</p> nas tecnologias mais modernas de desenvolvimento web e mobile',
     modules: '20',
     price: 'Pago',
